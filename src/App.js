001d4563798b1a07fcc39e83349a2b0b3ba027e6
Cjorozco <jose@draxo.co>
2021-09-10 13:08:31 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleEmailChange ({target: { value }}){
+    this.setState({email: value});
+  }
+  handlePasswordChange ({target: { value }}) {
+    this.setState({password: value});
+  }
+  handleFormSubmit (event) {
+    alert(`Usuario: ${this.state.email}, Password: ${this.state.password}`)
+    event.preventDefault();
+  };
+
+  render () {
+    return (
+      <div className='App'>
+        <form onSubmit={this.handleFormSubmit}>
+          <h2>Login</h2>
+          <label>
+            Email
+            <input type='email' value={this.state.email} onChange={this.handleEmailChange} />
+          </label>
+          <label>
+            Password
+            <input type='password' value={this.state.password} onChange={this.handlePasswordChange} />
+          </label>
+          <button type='submit'>
+            Enter
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
